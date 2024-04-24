d95c908c0cebe0dae69261f69a122018917cd156
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-04-24 19:08:22 +0200

    practice

---
+++ week1/functions.py
@@ -0,0 +1,32 @@
+def square(num):
+    return num**2
+
+
+def square_print(num):
+    print(num**2)
+
+
+# print(square(5))
+# square_print(5)
+#
+# a = square(10) + 10
+# print(a)
+# b = square_print(10) + 10
+
+def multiply(num1, num2):
+    return num1*num2
+
+
+# print(multiply("sad", 3))
+# print(multiply(2, 4))
+def power(base, expotent):
+    return base**expotent
+
+
+# print(power(3, 4))
+def convert_to_usd(euros):
+    return euros/0.82
+
+# print(convert_to_usd(100))
+
+
