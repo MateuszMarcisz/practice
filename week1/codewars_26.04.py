64cdaeeeede5f974ba001aed5a4f026e4e20fe0e
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-04-26 12:30:12 +0200

    some hard stuff

--- week1/codewars_26.04.py
+++ week1/codewars_26.04.py
@@ -16,6 +16,67 @@ def digital_root(n):
         return sum([int(i) for i in str(n)])
 
 
-print(digital_root(493193768768))
-print(digital_root(942))
-print(digital_root(16))
+# print(digital_root(493193768768))
+# print(digital_root(942))
+# print(digital_root(16))
+
+# lst = [[1,2,3,4,5,6],[9,3,4,5,6,7,8]]
+# for x in lst:
+#     for y in x:
+#         print(y)
+#
+# print([x for x in lst])
+# print([y for x in lst for y in x])
+
+"""Jack really likes his number five:
+the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+  3 -->    15  (  3 * 5¹)
+ 10 -->   250  ( 10 * 5²)
+200 --> 25000  (200 * 5³)
+  0 -->     0  (  0 * 5¹)
+ -3 -->   -15  ( -3 * 5¹)"""
+
+
+def multiply(n):
+    return n * 5 ** (len(str(n))) if n > -1 else n * 5 ** (len(str(n)) - 1)
+
+
+# print(multiply(-3))
+# print(multiply(3))
+# print(multiply(10))
+# print(multiply(200))
+
+# """Given a logarithm base X and two values A and B, return a sum of logratihms with the base X:
+# log⁡XA+log⁡XB \log_XA + \log_XB logX​A+logX​B."""
+
+import math
+
+
+def logs(x, a, b):
+    return math.log(a * b, x)
+
+#
+# print(logs(10, 1000, 10))
+
+# 1000 = 10^3,
+# log10(1000) = 3
+
+"""Write a function that accepts an array of 10 integers (between 0 and 9), 
+that returns a string of those numbers in the form of a phone number.
+Example
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+"""
+def create_phone_number(n):
+    # z = []
+    # for num in n[:3]:
+    #     z.append(str(num))
+    # return "".join(z)
+    return f'({"".join(str(num) for num in n[:3])}) {"".join(str(num) for num in n[3:6])}-{"".join(str(num) for num in n[6:])}'
+    # return f"{"".join(str(num) for num in n[:3]}, n[3:6], n[6:]"
+    # return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+
+# print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
