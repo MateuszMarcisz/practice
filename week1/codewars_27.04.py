c7a8f379a9a67f554340101c375e3ee602a345d6
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-04-28 11:34:07 +0200

    codewars 27.04

--- week1/codewars_27.04.py
+++ week1/codewars_27.04.py
@@ -21,7 +21,7 @@ Examples
 def find_outlier(integers):
     # that is too much, but I had to do it haha
     return [x for x in integers if x % 2 == 0][0] if len([x for x in integers if x % 2 == 0]) == 1 else \
-    [x for x in integers if x % 2 == 1][0]
+        [x for x in integers if x % 2 == 1][0]
     # even = [x for x in integers if x % 2 == 0]
     # odd = [x for x in integers if x % 2 == 1]
     # return even[0] if len(even) == 1 else odd[0]
@@ -45,3 +45,96 @@ def move_zeros(lst):
 
 
 # print(move_zeros([1, 0, 1, 2, 0, 1, 3, 0, 0, 12, 166]))
+
+"""The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+if that character appears only once in the original string, or ")" 
+if that character appears more than once in the original string. 
+Ignore capitalization when determining if a character is a duplicate.
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" """
+
+
+def duplicate_encode(word):
+    word = word.lower()
+    wordz = ''.join("(" if word.count(i) == 1 else ")" for i in word)
+    # wordz = ""
+    # for i in word:
+    #     if word.count(i) == 1:
+    #         wordz += "("
+    #     else:
+    #         wordz += ")"
+    return wordz
+
+
+# print(duplicate_encode("din"))
+# print(duplicate_encode("recede"))
+# print(duplicate_encode("Success"))
+# print(duplicate_encode("(( @"))
+
+"""You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. 
+The city provides its citizens with a Walk Generating App on their phones -- 
+everytime you press the button it sends you an array of one-letter strings representing directions to walk 
+(eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction)
+ and you know it takes you one minute to traverse one city block, 
+ so create a function that will return true if the walk the app gives you will take you exactly ten minutes 
+ (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+Note:
+you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). 
+It will never give you an empty array (that's not a walk, that's standing still!)."""
+
+
+def is_valid_walk(walk):
+    return True if walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w') and len(
+        walk) == 10 else False
+
+
+# print(is_valid_walk(['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'n', 's']))
+"""Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+which is the number of times you must multiply the digits in num until you reach a single digit.
+For example (Input --> Output):
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)"""
+
+
+def persistence(n):
+    count = 0
+    while n >= 10:
+        new_n = 1
+        for number in str(n):
+            new_n *= int(number)
+        n = new_n
+        count += 1
+    return count
+
+    # n = str(n)
+    # count = 0
+    # while len(n) > 1:
+    #     for i in range(len(n)-1):
+    #         count += 1
+    #         if "0" in n:
+    #             return count
+    #         n = "".join([f"{i}*" for i in n])[:-1]
+    #         n = eval(n)
+    #         n = str(n)
+    # else:
+    #     return count
+    # numberz = ""
+    # for i in n:
+    #     numberz += f"{i}*"
+    # while len(numberz) > 1:
+    #     return eval(numberz)
+
+
+
+# print(persistence(337874))
+# print(persistence(4))
+# print(persistence(11))
+# print(persistence(39))
+# print(persistence(1584))
+# print(persistence(999))
+# print(persistence(191805))
