ac58bff17bbe751f9f99b169e03c5de79d627692
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-04-29 15:35:10 +0200

    just one more...

--- week2/JScodewars.js
+++ week2/JScodewars.js
@@ -141,3 +141,25 @@ function disemvowel(str) {
 
 // const result = disemvowel("This website is for losers LOL!");
 // console.log(result);
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Additionally, if the number is negative, return 0.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+
+function solution(number){
+ if (number === 0) {
+     return 0;
+ } else {
+     let numbers = [];
+     for (let i = 1; i < number; i++) {
+         if (i % 3 === 0 || i % 5 === 0) {
+             numbers.push(i)
+         }
+     }
+     return (numbers).reduce((a, b) => a + b, 0);
+ }
+}
+const result = solution(10);
+console.log(result);
