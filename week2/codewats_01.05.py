ab56129fd85d16531c29d0203abbd92ded2875a9
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-05-01 15:31:17 +0200

    another kata5

--- week2/codewats_01.05.py
+++ week2/codewats_01.05.py
@@ -54,3 +54,58 @@ def printer_error(s):
 
 # print(printer_error("aaabbbbhaijjjm"))
 # print(printer_error("aaaxbbbbyyhwawiwjjjwwm"))
+
+"""ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+If the function is passed a valid PIN string, return true, else return false.
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false"""
+def validate_pin(pin):
+    return pin.isdigit() and len(pin) in (4, 6)
+
+
+"""Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+Examples:
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']"""
+
+def solution(s):
+    lst = []
+    while len(s) > 1:
+        lst.append(s[:2])
+        s = s[2:]
+    if len(s) == 1:
+        lst.append(f"{s}_")
+    return lst
+
+
+# print(solution('abdec'))
+
+"""Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+    HH = hours, padded to 2 digits, range: 00 - 99
+    MM = minutes, padded to 2 digits, range: 00 - 59
+    SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+You can find some examples in the test fixtures.
+"""
+def make_readable(seconds):
+    hh = (seconds // 3600)
+    if len(str(hh)) == 1:
+        hh = f'0{hh}'
+    mm = (seconds // 60) % 60
+    if len(str(mm)) == 1:
+        mm = f'0{mm}'
+    ss = seconds % 60
+    if len(str(ss)) == 1:
+        ss = f'0{ss}'
+    return f'{hh}:{mm}:{ss}'
+
+
+# print(make_readable(59))
+# print(make_readable(66))
+# print(make_readable(3599))
+# print(make_readable(3600))
+# print(make_readable(86400))
+# print(make_readable(359999))
