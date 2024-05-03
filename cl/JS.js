1533724925bf1b542ee86e98205866800eca6e5e
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-05-03 10:12:14 +0200

    JS, more like BS

--- cl/JS.js
+++ cl/JS.js
@@ -57,3 +57,56 @@ function pointless_function() {
 }
 console.log(pointless_function())
 
+function summing(array){
+   return array.reduce((a , b) => a +b);
+   // let sum = 0
+   // for (let i of array){
+   //     sum += i;
+   // }
+   // return sum
+}
+
+console.log(summing([1,2,3,4,5,6]))
+
+const more_pointless_nonesen = function(num1, num2, num3) {
+    // const why_would_anyone_would_need_this_kind_of_bs = Math.max(num1, num2, num3);
+    // return why_would_anyone_would_need_this_kind_of_bs
+    return Math.max(num1, num2, num3)
+}
+console.log(more_pointless_nonesen(4, 99, 44))
+
+function calculate_tip(amount, rating){
+    if (rating === 'Bardzo dobra obsługa'){
+        return amount*0.25
+    } else if (rating === 'Dobra obsługa'){
+        return amount*0.20
+    }else if (rating === 'Średnia obsługa'){
+        return amount*0.15
+    }else if (rating === 'Zła obsługa'){
+        return 0
+    } else {
+        return 'opis nieczytelny'
+    }
+}
+
+console.log(calculate_tip(200, 'Dobra obsługa'))
+
+function calculate_tip_proper(){
+    const amount = parseFloat(prompt('Podaj kwotę na rachunku: '));
+    const rating = prompt('Podaj opis jakości obsługi (Bardzo dobra obsługa, Dobra obsługa, Średnia obsługa, Zła obsługa): ');
+
+    if (rating === 'Bardzo dobra obsługa'){
+        alert('Napiwek do zapłaty: ' + (amount*0.25));
+    } else if (rating === 'Dobra obsługa'){
+        alert('Napiwek do zapłaty:' + (amount*0.20));
+    }else if (rating === 'Średnia obsługa'){
+        alert('Napiwek do zapłaty: ' + (amount*0.15));
+    }else if (rating === 'Zła obsługa'){
+        alert('Napiwek do zapłaty: 0');
+    }
+    else {
+        alert('opis nieczytelny');
+    }
+}
+
+console.log(calculate_tip_proper())
