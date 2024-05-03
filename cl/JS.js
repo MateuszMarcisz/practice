58f3039b52f9b25a6cd2293c26cf207451304349
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-05-03 09:43:41 +0200

    one more JS bs

--- cl/JS.js
+++ cl/JS.js
@@ -28,8 +28,32 @@ console.log(factorial(8))
 function sum_(num1, num2){
     let nums = 0
     for (let num = num1; num <=num2; num++ ) {
-        nums += num
+        nums += num;
     }
-    return nums
+    return nums;
 }
 console.log(sum_(8, 20))
+
+const numbers = [4,4,1,2,5,40];
+let avg = (numbers.reduce((a,b) => a + b)) / numbers.length;
+
+console.log(avg)
+
+function pointless_function() {
+    const pointless = [];
+    const nonesens = [];
+    for (let i = 1; i <=10; i++) {
+        pointless.push(Math.floor(Math.random() * 60) + 1);
+    }
+    for (let i of pointless) {
+        if (i % 2 === 1) {
+            nonesens.push(i +1);
+        }
+        else {
+            nonesens.push(i);
+        }
+    }
+    return nonesens
+}
+console.log(pointless_function())
+
