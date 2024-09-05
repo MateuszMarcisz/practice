0393901a8b8d8bb45865d41a6fcbecedda7e5cf6
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-09-05 17:47:01 +0200

    codewars python training

---
+++ codewars.5.09.py
@@ -0,0 +1,117 @@
+cw = '''Complete the method which accepts an array of integers, and returns one of the following:
+
+    "yes, ascending" - if the numbers in the array are sorted in an ascending order
+    "yes, descending" - if the numbers in the array are sorted in a descending order
+    "no" - otherwise
+
+You can assume the array will always be valid, and there will always be one correct answer.
+'''
+
+def is_sorted_and_how(arr):
+    if arr == sorted(arr):
+        return 'yes, ascending'
+    if arr[::-1] == sorted(arr):
+        return 'yes, descending'
+    return 'no'
+
+
+"""Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+Examples
+Valid arrays
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+Invalid arrays
+
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+Remarks
+
+    a or b might be [] or {} (all languages except R, Shell).
+    a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+"""
+
+def comp(array1, array2):
+    if not array1 or not array2:
+        return False
+    if array1 is None or array2 is None:
+        return False
+
+
+
+    s1 = sorted(array1)
+    s2 = sorted(array2)
+
+    for i in range(len(s1)):
+        if s1[i]**2 != s2[i]:
+            return False
+    return True
+
+
+"""Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+"""
+
+def sort_by_length(arr):
+    return sorted(arr, key=lambda x: len(x))
+
+
+# print(sort_by_length(["Telescopes", "Glasses", "Wyes", "Monocles"]))
+
+
+"""My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
+
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+
+For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+
+Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+Example:
+
+"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+
+"100 180 90 56 65 74 68 86 99"
+
+When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+
+180 is before 90 since, having the same "weight" (9), it comes before as a string.
+
+All numbers in the list are positive numbers and the list can be empty.
+Notes
+
+    it may happen that the input string have leading, trailing whitespaces and more than a unique whitespace between two consecutive numbers
+    For C: The result is free"""
+
+def order_weight(strng):
+    # string = map(int, strng.split())
+    string = strng.split()
+
+    return " ".join(sorted(string, key= lambda x: (sum(int(y) for y in x), x)))
+
+print(order_weight("56 65 74 100 99 68 86 180 90"))
